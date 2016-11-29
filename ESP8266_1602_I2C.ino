2d54e55ba987aee559d01649d2f1cc5c18d51a35
Pelikano <leon@berkenheuvel.com>
2016-11-29 21:36:36 +0100

    pin layouts

--- ESP8266_1602_I2C.ino
+++ ESP8266_1602_I2C.ino
@@ -1,6 +1,18 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+// pin config nodemcu
+// scl = D1
+// sda = D2 
+
+// pin config esp8266 01
+// scl = GPIO2
+// sda = GPIO0 
+
+// pin config esp8266 ESP-12
+// scl = GPIO5
+// sda = GPIO4 
+
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
